bb653dfcce1374e50188a7024a2e59be43986131
Dongseon Lee <eastline@maycan.co.kr>
2021-06-14 11:23:00 +0900

    #Day22. AppLoading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = async () => {
+    const images = [require("./assets/logo.png")];
+    const fontsToLoad = [Ionicons.font];
+    const cacheImages = images.map((image) => Asset.loadAsync(image));
+    const cacheFonts = fontsToLoad.map((font) => Font.loadAsync(font));
+    await Promise.all<any>([...cacheImages, ...cacheFonts]);
+  };
+
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
